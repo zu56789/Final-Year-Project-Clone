0fa49219bde180e7602140ce74756c470ea5073d
Zuhayr <zuhayrmohsen@gmail.com>
2023-10-27 20:59:30 +0100

    Created GameThread class

---
+++ src/game/GameThread.java
@@ -0,0 +1,11 @@
+package game;
+
+public class GameThread implements Runnable{
+
+  @Override
+  public void run() {
+    // TODO Auto-generated method stub
+    
+  }
+
+}
---
+++ src/test/TestGameThread.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import game.GameThread;
+
+public class TestGameThread {
+
+  @Test
+  public void test() {
+    GameThread game = new GameThread();
+  }
+
+}
