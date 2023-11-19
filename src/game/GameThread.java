685ea53828245f6d13ed5ae8955e31e9f9ff8afe
Zuhayr <zuhayrmohsen@gmail.com>
2023-11-19 23:44:28 +0000

    Refactored gameThread to implement the move validator class

--- src/game/GameThread.java
+++ src/game/GameThread.java
@@ -2,6 +2,8 @@ package game;
 
 import java.util.ArrayList;
 import gui.Board;
+import move.Move;
+import moveValidator.MoveValidator;
 import pieces.Piece;
 import player.Player;
 
@@ -12,6 +14,7 @@ public class GameThread implements Runnable{
   private boolean player1turn;
   private boolean gameOver;
   private int count;
+  private MoveValidator moveValidator;
   
   public GameThread(Player player1, Player player2) {
     this.player1 = player1;
@@ -20,6 +23,7 @@ public class GameThread implements Runnable{
     this.player1turn = true;
     this.gameOver = false;
     this.count = 0;
+    moveValidator = MoveValidator.getInstance();
   }
   
   @Override
@@ -27,15 +31,128 @@ public class GameThread implements Runnable{
     this.board.drawPieces(this.getPlayer1Pieces(),this.getPlayer2Pieces());
     
     while(!this.gameOver) {
-      
       if (this.player1turn) {
-        this.simulatep1Move();
+        System.out.println("White turn");
+        this.simulateTurn(player1turn);
+        
       }
       else {
-        this.simulatep2Move();
+        System.out.println("Black turn");
+        this.simulateTurn(player1turn);
       }
-  
     }
+    
+    
+  }
+  
+  
+  public void simulateTurn(boolean whiteturn) {
+    
+    while (!this.getBoard().Pressed()) {
+      Thread.yield();
+    }
+    
+    while (!this.getBoard().Released()) {
+      Thread.yield();
+    }
+    
+    if (this.getBoard().Pressed()) {
+      
+      int x1 = this.getBoard().getPressedX();
+      int y1 = this.getBoard().getPressedY();
+      
+      Piece piece1 = this.getBoard().getPiece(x1, y1);
+      
+      
+      
+      if (this.getBoard().Released()) {
+        int x2 = this.getBoard().getReleasedX();
+        int y2 = this.getBoard().getReleasedY();
+        
+        Piece piece2 = this.getBoard().getPiece(x2, y2);
+        // work on capture stuff later
+        
+        
+        System.out.println(x2 + " " + y2);
+        
+        
+        Move move = new Move(x1,y1,x2,y2);
+        
+        if (moveValidator.validMove(move, this.getBoard(), whiteturn)) {
+          this.updateBoard(piece1, x2, y2, whiteturn);
+          
+          if (whiteturn) {
+            this.player1turn = false;
+          }
+          else {
+            this.player1turn = true;
+          }
+        }
+        
+        else {
+          System.out.println("Invalid move");
+          this.getBoard().setPressed(false);
+          this.getBoard().setReleased(false);
+          simulateTurn(whiteturn);
+        }
+      }
+      
+      
+      
+    }
+    
+    else {
+      
+      this.getBoard().setPressed(false);
+      this.getBoard().setReleased(false);
+      
+      simulateTurn(whiteturn);
+    }
+    
+    
+    
+  }
+  
+  
+  public void updateBoard(Piece piece1, int x2, int y2, boolean whiteturn) {
+    
+    if (whiteturn) {
+      
+      ArrayList<Piece> list = this.getPlayer1Pieces();
+      list.remove(piece1);
+      
+      piece1.setColumn(x2);
+      piece1.setRow(y2);
+      
+      list.add(piece1);
+      
+      this.player1.setPieces(list);
+      
+      this.board.drawPieces(this.player1.getPieces(), this.player2.getPieces());
+      
+      
+    }
+    
+    else {
+      
+      ArrayList<Piece> list = this.getPlayer2Pieces();
+      list.remove(piece1);
+      
+      piece1.setColumn(x2);
+      piece1.setRow(y2);
+      
+      list.add(piece1);
+      
+      this.player2.setPieces(list);
+      
+      this.board.drawPieces(this.player1.getPieces(), this.player2.getPieces());
+      
+      
+      
+      
+    }
+    
+    
     
   }
   
@@ -51,72 +168,7 @@ public class GameThread implements Runnable{
     return this.player2.getPieces();
   }
   
-  public void simulatep1Move() {
-    
-    while (!this.board.Pressed()) {
-      Thread.yield();
-    }
-    
-    while (this.board.getSelectedPiece().isBlack()) {
-      Thread.yield();
-    }
-    
-    
-    
-    while (!this.board.Released()) {
-      Thread.yield();
-    }
-    
-    count++;
-    System.out.println(this.board.getSelectedPiece().getName() + " " + this.board.getSelectedPiece().isBlack());
-    
-    if (this.board.getReleasedX() < 8 && this.board.getReleasedY() < 8) {
-      Piece pressed = this.board.getSelectedPiece();
-      int col = this.board.getReleasedX();
-      int row = this.board.getReleasedY();
-      ArrayList<Piece> temp = this.getPlayer1Pieces();
-      temp.remove(pressed);
-      pressed.setColumn(col);
-      pressed.setRow(row);
-      this.player1.setPieces(temp);
-      this.board.drawPieces(this.player1.getPieces(), this.player2.getPieces());
-    }
-    
-    this.player1turn = false;
-  }
   
-  public void simulatep2Move() {
-    
-    while (!this.board.Pressed()) {
-      Thread.yield();
-    }
-    
-    while (!this.board.getSelectedPiece().isBlack()) {
-      Thread.yield();
-    }
-    
-    
-    
-    while (!this.board.Released()) {
-      Thread.yield();
-    }
-    
-    count++;
-    System.out.println(this.board.getSelectedPiece().getName() + " " + this.board.getSelectedPiece().isBlack());
-    
-    if (this.board.getReleasedX() < 8 && this.board.getReleasedY() < 8) {
-      Piece pressed = this.board.getSelectedPiece();
-      int col = this.board.getReleasedX();
-      int row = this.board.getReleasedY();
-      ArrayList<Piece> temp = this.getPlayer2Pieces();
-      temp.remove(pressed);
-      pressed.setColumn(col);
-      pressed.setRow(row);
-      this.player2.setPieces(temp);
-      this.board.drawPieces(this.player1.getPieces(), this.player2.getPieces());
-    }
-    
-    this.player1turn = true;
-  }
+  
   
 }
